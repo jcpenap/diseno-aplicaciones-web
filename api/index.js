fdc4860a21d043167b1d490b0e0aa213683e0b5c
Jesus Estrada <jesuestrada@gmail.com>
2020-07-18 09:22:50 -0500

    middleware

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 const express = require ('express');
 const router = express.Router();
-
+const logger = require('./middlewares/logger');
 const users = require('./routers/users');
 
+router.use(logger);
 router.use('/users', users);
 
 module.exports = router;
--- api/routers/users/index.js
+++ api/routers/users/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./../../controllers/users');
 
-router.get('/', controller.getAll);
+router.route('/')
+    .get(controller.getAll)
+    .post(controller.newUser)
+    .delete(controller.deleteUser);
+
+router.route('/:id')
+    .get(controller.getUser)
+    .put(controller.updateUser);
 
 module.exports = router;
