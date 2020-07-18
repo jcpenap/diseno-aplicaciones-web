29bb1e594d7e3e3c5693615e4f1476befca74519
Jesus Estrada <jesuestrada@gmail.com>
2020-07-18 10:34:34 -0500

    model

--- api/controllers/users/index.js
+++ api/controllers/users/index.js
@@ -1,11 +1,38 @@
+const User = require('./../../models/users');
+
 const getAll = (req, res) =>{
-    res.send("Lista de usuarios");
+    User.find({})
+    .then((response)=>{
+        res.status(200).send(response);
+    })
+    .catch((err)=>{
+        res.sendStatus(500);
+    })
 };
 const getUser = (req, res) => {
     res.send("Página de usuario");
 };
 const newUser = (req, res) => {
-    res.send("Crear usuario");
+    const user = {
+        name: req.body.name,
+        age: req.body.age,
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+        telephone: req.body.telephone
+    };
+    if(user.name && user.age && user.username && user.password && user.email){
+        const object = new User(user);
+        object.save()
+        .then(()=>{
+            res.status(201).send("El usuario fue creado");
+        })
+        .catch((err)=>{
+            res.sendStatus(500);
+        })
+    }else{
+        res.sendStatus(500);
+    }
 };
 const updateUser = (req, res) => {
     res.send("Actualizar usuario");
