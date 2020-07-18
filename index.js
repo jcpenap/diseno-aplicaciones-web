c1522d6c95fb1ad38d2cc1eb061dcc68289f28e5
Jesus Estrada <jesuestrada@gmail.com>
2020-07-18 10:01:46 -0500

    mongoose

--- index.js
+++ index.js
@@ -1,47 +1,19 @@
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const api = require('./api');
 
 app.use(express.json());
 app.use('/api', api);
+//protocol://user:password@host:port/resource
+mongoose.connect('mongodb://localhost/twitter', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, ()=>{
+    console.log("Conectado a la base de datos");
+});
 
 app.listen(3000, () => {
     console.log("Servidor iniciado");
 });
-
-/*
-app.get('/', (req, res) =>{
-    res.status(200).send(`Hola mundo ${date()}`);
-});
-app.get('/users', (req, res)=>{
-    res.status(200).send(users);
-});
-app.get('/users/:id', (req, res)=>{
-    const id = req.params.id;
-    if(id>0){
-        res.status(200).send(`Página del usuario ${id}`);
-    }else{
-        res.sendStatus(400);
-    }
-});
-
-app.get('/bmi/:weight/:height', (req, res)=>{
-    const weight = req.params.weight;
-    const height = req.params.height;
-    const bmiResult = bmi(weight, height);
-    bmiResult === -1 ?
-        res.sendStatus(500)
-    :    
-        res.status(200).send(`bmi: ${bmiResult}`);
-});
-
-app.post('/users', (req, res)=>{
-    const user = {
-        name: req.body.name,
-        lastname: req.body.lastname
-    };
-    users.push(user);
-    res.status(200).send(`El usuario ${user.name} fue creado`);    
-});
-*/
